535d6a13a637dbd2b25654a4c1f8c99782f94242
Cláudia Oliveira <113480999+claudiadecopas@users.noreply.github.com>
2023-03-06 23:43:14 -0300

    2023-03-06-commit-antes-do-login
    2023-03-06-commit-antes-do-login

--- consumo-api/src/app/app.module.ts
+++ consumo-api/src/app/app.module.ts
@@ -1,26 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-/*Importações do Angular Material*/
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+/*Importações Angular Material*/
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
+import { RestritoRoutingModule } from './restrito/restrito-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { RestritoComponent } from './restrito/restrito.component';
 import { AtualizaProdutoComponent } from './restrito/atualiza-produto/atualiza-produto.component';
 import { CadastroProdutoComponent } from './restrito/cadastro-produto/cadastro-produto.component';
 import { ListaProdutoComponent } from './restrito/lista-produto/lista-produto.component';
 import { MenuRestritoComponent } from './restrito/menu-restrito/menu-restrito.component';
+
 
 @NgModule({
   declarations: [
@@ -29,10 +35,11 @@ import { MenuRestritoComponent } from './restrito/menu-restrito/menu-restrito.co
     LoginComponent,
     MenuComponent,
     RodapeComponent,
+    RestritoComponent,
     AtualizaProdutoComponent,
     CadastroProdutoComponent,
     ListaProdutoComponent,
-    MenuRestritoComponent
+    MenuRestritoComponent,
   ],
   imports: [
     BrowserModule,
@@ -45,7 +52,11 @@ import { MenuRestritoComponent } from './restrito/menu-restrito/menu-restrito.co
     MatIconModule,
     MatInputModule,
     MatMenuModule,
-    MatToolbarModule,   
+    MatToolbarModule,
+    RestritoRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
